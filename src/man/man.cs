3dc80878f08c5f009e49fb2f456d069bc0ddc5f4
droog126 <59406205+droog126@users.noreply.github.com>
2023-10-31 23:39:18 +0800

    整理好文件夹，修复了debug相机

--- src/man/man.cs
+++ src/man/man.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Man : CharacterBody3D
+public partial class man : CharacterBody3D
 {
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
@@ -10,46 +10,35 @@ public partial class Man : CharacterBody3D
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
 
-	public override void _Ready()
-	{
-		GD.Print("?????");
-	}
 
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
-		GD.Print("?????");
-
-	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 
-		GD.Print("??");
-		// Vector3 velocity = Velocity;
+		Vector3 velocity = Velocity;
 
-		// if (!IsOnFloor())
-		// 	velocity.Y -= gravity * (float)delta;
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
 
-		// if (Input.IsActionJustPressed("space") && IsOnFloor())
-		// 	velocity.Y = JumpVelocity;
+		if (Input.IsActionJustPressed("space") && IsOnFloor())
+			velocity.Y = JumpVelocity;
 
 
-		// Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		// Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		// if (direction != Vector3.Zero)
-		// {
-		// 	velocity.X = direction.X * Speed;
-		// 	velocity.Z = direction.Z * Speed;
-		// }
-		// else
-		// {
-		// 	velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		// 	velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		// }
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
 
-		// Velocity = velocity;
-		// MoveAndSlide();
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
 
--- system/camera/Camera3D.gd
+++ system/camera/Camera3D.gd
@@ -1,10 +1,5 @@
 extends Node3D
 @onready var camera = $Camera3D
-
-
-
-
-
 var CameraMode = _G.CameraMode
 
 func _ready():
@@ -14,7 +9,12 @@ func debug():
 	camera.current = true;
 	if Input.is_action_just_pressed('f2'):
 		_G.cameraMode_add_circle() 
-		
+	if  Input.is_action_just_pressed('mouse_right'):
+		if _G.cameraMode!= CameraMode.Static:
+			_G.cameraMode = CameraMode.Static
+		else:
+			_G.cameraMode = CameraMode.Debug
+
 		
 func _input(event):
 	debug()
@@ -22,40 +22,29 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		pass
 	elif _G.cameraMode == CameraMode.Debug:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		if event is InputEventMouseMotion:
 			rotate_y(-event.relative.x * .005)
 			camera.rotate_x(-event.relative.y * .005)
 			camera.rotation.x = clamp(camera.rotation.x,-PI/2,PI/2)
 	elif _G.cameraMode == CameraMode.ThirdPerson:
-
 		if _G.cameraTarget == null:
 			_G.cameraMode = CameraMode.Static
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			
-#			Log.debug(str('第三人称，没找到附身对象'))
-
+			print("没有找到附身对象")
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
 		if event is InputEventMouseMotion:	
-
 			camera.rotate_x(-event.relative.y * .005)
-
-#			rotation.x = clamp(camera.rotation.x,-PI/2,PI/2)
 			pass
-
-	
 	elif _G.cameraMode == CameraMode.FirstPerson:
-
 		if _G.cameraTarget == null:
 			_G.cameraMode = CameraMode.Static
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-#			Log.debug(str('第一人称，没找到附身对象'))
+			print("没有找到附身对象")
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		
 		if event is InputEventMouseMotion:
-
 			camera.rotate_x(-event.relative.y * .005)
 			camera.rotation.x = clamp(camera.rotation.x,-PI/2,PI/2)
 			pass
@@ -71,28 +60,13 @@ func _process(delta):
 	#	var direction = Vector3(x,y,z); 火车
 		# 含义是啥
 		var direction = (transform.basis * Vector3(x,y,z)).normalized()
-		global_transform.origin += direction * .05;
-
-		
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-			mouse_switch = !mouse_switch
-			if(mouse_switch):
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			else:
-				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-				
+		global_transform.origin += direction * .05;	
 	elif _G.cameraMode == CameraMode.ThirdPerson:
 		global_position = _G.cameraTarget.global_position + Vector3(0,1,1) * 5
-		
-		
 		look_at(_G.cameraTarget.global_position);
-
-
-		
 		pass
 	elif _G.cameraMode == CameraMode.FirstPerson:
 		global_transform.origin = _G.cameraTarget.global_position + Vector3(0,0.4,-0.2).rotated(Vector3(0,1,0),_G.cameraTarget.rotation.y)
 		rotation.y = _G.cameraTarget.rotation.y
-
 		pass
 
