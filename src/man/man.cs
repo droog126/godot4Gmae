b220e2d0f14bcb48495422673440227a58c892da
droog126 <59406205+droog126@users.noreply.github.com>
2023-11-01 00:52:43 +0800

    s

--- src/man/man.cs
+++ src/man/man.cs
@@ -1,29 +1,22 @@
 using Godot;
 using System;
-
 public partial class man : CharacterBody3D
 {
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	public double jumpStrength = 7;
+	public JumpClass jumpClass;
+	public Vector3 velocity;
 
-
-
+	public override void _Ready()
+	{
+		jumpClass = new JumpClass(jumpCallback);
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-
-		Vector3 velocity = Velocity;
-
-		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
-
-		if (Input.IsActionJustPressed("space") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-
+		// xz
 		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
@@ -37,9 +30,72 @@ public partial class man : CharacterBody3D
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
 		}
 
-		Velocity = velocity;
+		//y
+		jumpClass.Step(delta);
+
+		if (!IsOnFloor())
+		{
+			velocity.Y -= gravity * (float)delta;
+		}
+		else
+		{
+			jumpClass.Reset();
+			Scale = new Vector3(1.25f, 0.75f, 1.25f);
+		}
+
+		GD.Print(velocity, Velocity);
+		Velocity = Velocity.Lerp(velocity, (float)(delta * 10.0));
+		Scale = Scale.Lerp(Vector3.One, (float)(delta * 5.0));
 		MoveAndSlide();
+	}
+
+	public void jumpCallback()
+	{
+		velocity.Y = (float)jumpStrength;
+		Scale = new Vector3(0.5f, 1.5f, 0.5f);
 	}
 }
 
 
+public class JumpClass
+{
+	public bool jumpSingle = false;
+	public bool jumpDouble = false;
+	public Action jumpCallback;
+
+	public JumpClass(Action jumpCallback)
+	{
+		this.jumpCallback = jumpCallback;
+		jumpSingle = true;
+		jumpDouble = true;
+	}
+
+
+	public void Step(double delta)
+	{
+		if (Input.IsActionJustPressed("space"))
+		{
+
+			if (jumpSingle || jumpDouble)
+			{
+				jumpCallback();
+			}
+			if (jumpDouble)
+			{
+				jumpDouble = false;
+			}
+			if (jumpSingle)
+			{
+				jumpSingle = false;
+				jumpDouble = true;
+			}
+		}
+	}
+	public void Reset()
+	{
+		jumpSingle = true;
+		jumpDouble = true;
+	}
+
+
+}
